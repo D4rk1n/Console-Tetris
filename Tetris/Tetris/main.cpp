5918d6a717cc14124c50c966597b4af840bee9af
Darkin <darkinb255@gmail.com>
2019-05-26 01:25:06 +0200

    proto Main

--- Tetris/Tetris/main.cpp
+++ Tetris/Tetris/main.cpp
@@ -1,39 +1,131 @@
-#include <bits/stdc++.h>
+﻿#include <bits/stdc++.h>
 #include <ctime>
 #include <chrono>
+#include <conio.h>
+#include <Windows.h>
+#include <fcntl.h>
+#include <io.h>
+#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77
 using namespace std::chrono;
 using namespace std;
+void MenuTetris()
+{
+	_setmode(_fileno(stdout),_O_U16TEXT);
+	wprintf(L"\n ▀▀█▀▀ ▒█▀▀▀ ▀▀█▀▀ ▒█▀▀█ ▀█▀ ▒█▀▀▀█\n ░▒█░░ ▒█▀▀▀ ░▒█░░ ▒█▄▄▀ ▒█░ ░▀▀▀▄▄\n ░▒█░░ ▒█▄▄▄ ░▒█░░ ▒█░▒█ ▄█▄ ▒█▄▄▄█\n\n ");
 
+	_setmode(_fileno(stdout), _O_TEXT);
+}
 int main()
 {
-	steady_clock::time_point s = steady_clock::now();
-	for (size_t i = 0; i < 20; i++) {
+
+	int c = 0;
+	int choice = 0;
+	bool exit = false;
+	while (!exit)
+	{
+		
 		steady_clock::time_point t1 = steady_clock::now();
 		steady_clock::time_point t2 = steady_clock::now();
 		duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-		if (i <= 18) {
-			int j = 0;
-			for (j; j < i; j++) {
-				cout << "|| .. .. .. .. .. .. .. ||\n";
-			}
-			cout << "|| .. .. .. [] .. .. .. ||\n|| .. .. [] [] [] .. .. ||\n";
-
-			for (size_t z = 18 - i; z > 0; z--) {
-				cout << "|| .. .. .. .. .. .. .. ||\n";
-			}
-			cout << "^^^^^^^^^^^^^^^^^^^^^^^^^^\n==== S C O R E : 0000 ====\n^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
-			while (time_span.count() < .5) {
-				t2 = steady_clock::now();
-				time_span = duration_cast<duration<double>>(t2 - t1);
-			}
-			if (i < 18) system("cls");
+		MenuTetris();
+		if (choice)
+		{
+			cout << setw(12) << "  " << "S T A R T" << "  " << endl;
+			cout << setw(13) << "<<" << "E X I T" << ">>" << endl;
 		}
+		if (!choice)
+		{
+			cout << setw(12) << "<<" << "S T A R T" << ">>" << endl;
+			cout << setw(13) << "  " << "E X I T" << "  " << endl;
+		}
+		while (time_span.count() < .5) {
+			t2 = steady_clock::now();
+			time_span = duration_cast<duration<double>>(t2 - t1);
+			if (_kbhit())
+			{
+				c = _getch();
+				if (c == UP || c == DOWN)
+				{
+					choice = (choice + 1) % 2;
+					system("cls");
+					MenuTetris();
+					if (choice)
+					{
+						cout << setw(12) << "  " << "S T A R T" << "  " << endl;
+						cout << setw(13) << "<<" << "E X I T" << ">>" << endl;
+					}
+					if (!choice)
+					{
+						cout << setw(12) << "<<" << "S T A R T" << ">>" << endl;
+						cout << setw(13) << "  " << "E X I T" << "  " << endl;
+					}
+				}
+				if (GetKeyState(VK_SPACE) & 0x8000)
+				{
+					exit = true;
+					break;
+				}
+			}
+		}
+		system("cls");
+		MenuTetris();
+		cout << setw(12) << "  " << "S T A R T" << "  " << endl;
+		cout << setw(13) << "  " << "E X I T" << "  " << endl;
+		t1 = steady_clock::now();
+		t2 = steady_clock::now();
+		time_span = duration_cast<duration<double>>(t2 - t1);
+		while (time_span.count() < .5) {
+			t2 = steady_clock::now();
+			time_span = duration_cast<duration<double>>(t2 - t1);
+			if (GetKeyState(VK_UP) & 0x8000 || GetKeyState(VK_DOWN) & 0x8000)
+			{
+				break;
+			}
+			if (GetKeyState(VK_SPACE) & 0x8000)
+			{
+				exit = true;
+				break;
+			}
+	
+
+		}
+		system("cls");
+	}
+	if (!choice)
+	{
+		steady_clock::time_point s = steady_clock::now();
+		for (size_t i = 0; i < 20; i++) {
+			steady_clock::time_point t1 = steady_clock::now();
+			steady_clock::time_point t2 = steady_clock::now();
+			duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+			if (i <= 18) {
+				int j = 0;
+				for (j; j < i; j++) {
+					cout << "|| .. .. .. .. .. .. .. ||\n";
+				}
+				cout << "|| .. .. .. [] .. .. .. ||\n|| .. .. [] [] [] .. .. ||\n";
+
+				for (size_t z = 18 - i; z > 0; z--) {
+					cout << "|| .. .. .. .. .. .. .. ||\n";
+				}
+				cout << "^^^^^^^^^^^^^^^^^^^^^^^^^^\n==== S C O R E :" << setw(5) << "0";
+				cout << " ====\n^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
+				while (time_span.count() < .5) {
+					t2 = steady_clock::now();
+					time_span = duration_cast<duration<double>>(t2 - t1);
+				}
+				if (i < 18) system("cls");
+			}
+
+		}
+		steady_clock::time_point e = steady_clock::now();
+		duration<double>  final = duration_cast<duration<double>>(e - s);
 
 	}
-	steady_clock::time_point e = steady_clock::now();
-	duration<double>  final = duration_cast<duration<double>>(e - s);
-
-
+	
 
 
 	return 0;
