6ee15d401cf939f55fdb37c74e3bac4d249f7c38
Hassan Mohamed <hmibrahim1999@gmail.com>
2019-06-06 00:58:51 +0200

    efficient clearing

--- Tetris/Tetris/main.cpp
+++ Tetris/Tetris/main.cpp
@@ -1,16 +1,18 @@
-﻿#include <bits/stdc++.h>
+﻿#include <bits/stdc.h>
 #include <ctime>
 #include <chrono>
 #include <conio.h>
 #include <Windows.h>
 #include <fcntl.h>
 #include <io.h>
+using namespace std;
+using namespace std::chrono;
 #define UP 72
 #define DOWN 80
 #define LEFT 75
 #define RIGHT 77
-using namespace std::chrono;
-using namespace std;
+#define width 26
+#define height 24
 void MenuTetris()
 {
 	_setmode(_fileno(stdout),_O_U16TEXT);
@@ -24,12 +26,13 @@ int main()
 	int c = 0;
 	int choice = 0;
 	bool exit = false;
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 	while (!exit)
 	{
 		
 		steady_clock::time_point t1 = steady_clock::now();
 		steady_clock::time_point t2 = steady_clock::now();
-		duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+		duration<double> time_span = duration_cast<duration<double> >(t2 - t1);
 		MenuTetris();
 		if (choice)
 		{
@@ -43,14 +46,14 @@ int main()
 		}
 		while (time_span.count() < .5) {
 			t2 = steady_clock::now();
-			time_span = duration_cast<duration<double>>(t2 - t1);
+			time_span = duration_cast<duration<double> >(t2 - t1);
 			if (_kbhit())
 			{
 				c = _getch();
 				if (c == UP || c == DOWN)
 				{
 					choice = (choice + 1) % 2;
-					system("cls");
+					SetConsoleCursorPosition(console, { 0, 0 });
 					MenuTetris();
 					if (choice)
 					{
@@ -70,16 +73,16 @@ int main()
 				}
 			}
 		}
-		system("cls");
+		SetConsoleCursorPosition(console, { 0, 0 });
 		MenuTetris();
 		cout << setw(12) << "  " << "S T A R T" << "  " << endl;
 		cout << setw(13) << "  " << "E X I T" << "  " << endl;
 		t1 = steady_clock::now();
 		t2 = steady_clock::now();
-		time_span = duration_cast<duration<double>>(t2 - t1);
+		time_span = duration_cast<duration<double> >(t2 - t1);
 		while (time_span.count() < .5) {
 			t2 = steady_clock::now();
-			time_span = duration_cast<duration<double>>(t2 - t1);
+			time_span = duration_cast<duration<double> >(t2 - t1);
 			if (GetKeyState(VK_UP) & 0x8000 || GetKeyState(VK_DOWN) & 0x8000)
 			{
 				break;
@@ -92,15 +95,16 @@ int main()
 	
 
 		}
-		system("cls");
+		SetConsoleCursorPosition(console, { 0, 0 });
 	}
 	if (!choice)
 	{
+		system("cls");
 		steady_clock::time_point s = steady_clock::now();
 		for (size_t i = 0; i < 20; i++) {
 			steady_clock::time_point t1 = steady_clock::now();
 			steady_clock::time_point t2 = steady_clock::now();
-			duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+			duration<double> time_span = duration_cast<duration<double> >(t2 - t1);
 			if (i <= 18) {
 				int j = 0;
 				for (j; j < i; j++) {
@@ -115,14 +119,14 @@ int main()
 				cout << " ====\n^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
 				while (time_span.count() < .5) {
 					t2 = steady_clock::now();
-					time_span = duration_cast<duration<double>>(t2 - t1);
+					time_span = duration_cast<duration<double> >(t2 - t1);
 				}
-				if (i < 18) system("cls");
+				if (i < 18) SetConsoleCursorPosition(console, { 0, 0 });
 			}
 
 		}
 		steady_clock::time_point e = steady_clock::now();
-		duration<double>  final = duration_cast<duration<double>>(e - s);
+		duration<double>  final = duration_cast<duration<double> >(e - s);
 
 	}
 	
